eda4f3ceffd0f53f880f62ad8d9cd798ffb308ae
Rzkykhrllh <Rizkykirigaya@gmail.com>
2021-09-08 16:14:08 +0800

    find out section responsive finish

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import FindOut from "../parts/FindOut";
 import Footer from "../parts/Footer";
 import Hero from "../parts/Hero";
 
 function LandingPage() {
   return (
     <div>
-      {/* <Hero /> */}
+      <Hero />
+      <FindOut />
       <Footer />
     </div>
   );
---
+++ src/parts/FindOut.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./index.css";
+
+function FindOut() {
+  return (
+    <section className="px-8 py-36">
+      {/* content */}
+
+      <div className="flex flex-col justify-between px-10 py-20 text-white bg-no-repeat md:px-16 lg:px-20 md:flex-row gap-y-10 bg-findout">
+        <h3 className="text-3xl md:text-5xl md:text-left font-serif-display">
+          Find out more <br className="hidden md:block" />
+          about how we work
+        </h3>
+
+        <div className="flex justify-center my-auto">
+          <a href="#" className="px-5 py-3 border md:py-4">
+            HOW WE WORK
+          </a>
+        </div>
+        {/*  */}
+      </div>
+    </section>
+  );
+}
+
+export default FindOut;
